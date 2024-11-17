72044d36d180d8a10144b824b40ede474e5977f8
sky <3138875809@qq.com>
2024-11-17 20:16:13 +0800

    编写基于 Vert.x 实现的 web 服务器 VertxHttpServer，能够监听指定端口并处理请求。

--- example-provider/src/main/java/com/sky/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/sky/example/provider/EasyProviderExample.java
@@ -1,5 +1,8 @@
 package com.sky.example.provider;
 
+import com.sky.skyrpc.server.HttpServer;
+import com.sky.skyrpc.server.VertxHttpServer;
+
 /**
  * @author 胖了又胖的胖凯
  * @date 2024-11-17 19:25
@@ -8,6 +11,7 @@ package com.sky.example.provider;
 public class EasyProviderExample {
     public static void main(String[] args) {
         //提供服务
-
+        HttpServer httpServer=new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
---
+++ sky-rpc-easy/src/main/java/com/sky/skyrpc/server/VertxHttpServer.java
@@ -0,0 +1,39 @@
+package com.sky.skyrpc.server;
+
+import io.vertx.core.Vertx;
+
+/**
+ * @author 胖了又胖的胖凯
+ * @date 2024-11-17 20:03
+ * @description 编写基于 Vert.x 实现的 web 服务器 VertxHttpServer，能够监听指定端口并处理请求。
+ */
+public class VertxHttpServer implements HttpServer{
+    @Override
+    public void doStart(int port) {
+        //创建vertx实例
+        Vertx vertx= Vertx.vertx();
+
+        // 创建 HTTP 服务器
+        io.vertx.core.http.HttpServer httpServer= vertx.createHttpServer();
+
+        //监听端口并处理请求
+        httpServer.requestHandler(httpServerRequest -> {
+            //处理http请求
+            System.out.println("Received request: "+httpServerRequest.method());
+
+            //发送HTTP请求
+            httpServerRequest.response()
+                    .putHeader("content-type","text/plain")
+                    .end("Hello from Vert.x Http Server");
+        });
+
+        //启动HTTP服务器并监听指定端口
+        httpServer.listen(port,result->{
+           if(result.succeeded()){
+               System.out.println("Server is now listening on port");
+           }else{
+               System.err.println("Failed to start server: "+result.cause());
+           }
+        });
+    }
+}
