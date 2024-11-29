3e841e1b73ca2881f50d4da292d9596d851e5de6
sky <3138875809@qq.com>
2024-11-29 23:14:17 +0800

    封装半包粘包处理器

--- sky-rpc-easy/src/main/java/com/sky/skyrpc/server/VertxHttpServer.java
+++ sky-rpc-easy/src/main/java/com/sky/skyrpc/server/VertxHttpServer.java
@@ -1,6 +1,12 @@
 package com.sky.skyrpc.server;
 
+import com.sky.skyrpc.server.tcp.VertxTcpServer;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetServer;
+import io.vertx.core.parsetools.RecordParser;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Vertx HTTP 服务器
@@ -9,6 +15,7 @@ import io.vertx.core.Vertx;
  * @learn <a href="https://codefather.cn">编程宝典</a>
  * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
+@Slf4j
 public class VertxHttpServer implements HttpServer {
 
     /**
@@ -16,23 +23,58 @@ public class VertxHttpServer implements HttpServer {
      *
      * @param port
      */
+    @Override
     public void doStart(int port) {
         // 创建 Vert.x 实例
         Vertx vertx = Vertx.vertx();
 
-        // 创建 HTTP 服务器
-        io.vertx.core.http.HttpServer server = vertx.createHttpServer();
+        // 创建 TCP 服务器
+        NetServer server = vertx.createNetServer();
 
-        // 监听端口并处理请求
-        server.requestHandler(new HttpServerHandler());
+        // 处理请求
+        server.connectHandler(socket -> {
+            // 构造 parser
+            RecordParser parser = RecordParser.newFixed(8);
+            parser.setOutput(new Handler<Buffer>() {
+                // 初始化
+                int size = -1;
+                // 一次完整的读取（头 + 体）
+                Buffer resultBuffer = Buffer.buffer();
 
-        // 启动 HTTP 服务器并监听指定端口
+                @Override
+                public void handle(Buffer buffer) {
+                    if (-1 == size) {
+                        // 读取消息体长度
+                        size = buffer.getInt(4);
+                        parser.fixedSizeMode(size);
+                        // 写入头信息到结果
+                        resultBuffer.appendBuffer(buffer);
+                    } else {
+                        // 写入体信息到结果
+                        resultBuffer.appendBuffer(buffer);
+                        System.out.println(resultBuffer.toString());
+                        // 重置一轮
+                        parser.fixedSizeMode(8);
+                        size = -1;
+                        resultBuffer = Buffer.buffer();
+                    }
+                }
+            });
+
+            socket.handler(parser);
+        });
+
+        // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
             if (result.succeeded()) {
-                System.out.println("Server is now listening on port " + port);
+                log.info("TCP server started on port " + port);
             } else {
-                System.err.println("Failed to start server: " + result.cause());
+                log.info("Failed to start TCP server: " + result.cause());
             }
         });
+    }
+
+    public static void main(String[] args) {
+        new VertxTcpServer().doStart(8888);
     }
 }
